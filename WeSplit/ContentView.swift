017e0cc1f6a124a03e32a4cc03976034cb727240
Magdalena Struniawska <mag.sok@onet.pl>
2022-02-22 12:32:10 +0100

    complete first projekt

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+   let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+         Form {
+             Picker ("Select your student", selection:
+                        $selectedStudent) {
+                 ForEach(students, id: \.self) {
+                     Text($0)
+                 }
+             }
+            
+            
+            
+        
+        }
+        
     }
+            
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
+     }
+  }
 }
